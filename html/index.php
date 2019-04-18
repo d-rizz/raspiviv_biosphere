ee47f6d9bc6fe76c859266edab9eb97a63c02400
d-rizz <49502540+d-rizz@users.noreply.github.com>
2019-04-17 23:07:55 -0400

    Ol' Greenie

--- html/index.php
+++ html/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>RasPiViv.com - Home</title>
+	<title>Ol' Greenie</title>
 
     <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
